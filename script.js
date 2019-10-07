8f07b6700686d3a0439411d9e4cf7259deaebb5d
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-07 01:46:33 -0400

    Added timer to quiz function

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+var time = 120;
+
 function startQuiz() {
     document.querySelector("#titleScreen").style = "animation-play-state: running;"
+    document.querySelector(".navbar-text").textContent = "Time: " + time;
     setTimeout(function () {
         document.querySelector("#titleScreen").style = "display: none;";
         document.querySelector("#questionBlock").style = "display: block; animation-play-state: running;";
-    }, 400)
+    }, 400);
+    var timeLimit = setInterval(function () {
+        time--;
+        document.querySelector(".navbar-text").textContent = "Time: " + time;
+        if (time == 0) {
+            clearInterval(timeLimit);
+        }
+    }, 1000);
 }
 
 function changeQuestion() {
