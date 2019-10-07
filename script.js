d57f8700434e740119501155c5aeca2145d12986
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-07 14:10:09 -0400

    Specify changeQuestion to listen to all buttons

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-var time = 120;
+var time = 15 * questions.length;
+var questionNum = 0;
+var optionButtons = [document.querySelector("#quizOption1"), document.querySelector("#quizOption2"),
+document.querySelector("#quizOption3"), document.querySelector("#quizOption4")]
 
 function startQuiz() {
     document.querySelector("#titleScreen").style = "animation-play-state: running;"
     document.querySelector(".navbar-text").textContent = "Time: " + time;
+
     setTimeout(function () {
         document.querySelector("#titleScreen").style = "display: none;";
         document.querySelector("#questionBlock").style = "display: block; animation-play-state: running;";
@@ -16,12 +20,36 @@ function startQuiz() {
     }, 1000);
 }
 
+// changeQuestion operates only when the element clicked is a button
 function changeQuestion() {
-    document.querySelector("#questionBlock").className = "questionFadeOut";
-    setTimeout(function () {
-        document.querySelector("#questionBlock").className = "questionFadeIn";
-    }, 300);
+    // document.querySelector("#questionBlock").className = "questionFadeOut";
+    // setTimeout(function () {
+    //     document.querySelector("#questionBlock").className = "questionFadeIn";
+    // }, 300);
+    if (event.target.nodeName == "BUTTON") {
+        // If there's a value in the button (ie. an answer, check if it's correct)
+        if (event.target.value){
+            checkAnswer(event.target.value);
+        }
+        var questionInfo = questions[questionNum];
+
+        if (questionInfo == undefined) {
+            console.log(`There's no questions left...!`);
+            return;
+        }
+        document.querySelector("#questionPrompt").textContent = questionInfo.title;
+        for (var i = 0; i < optionButtons.length; i++) {
+            optionButtons[i].textContent = questionInfo.choices[i];
+            optionButtons[i].value = questionInfo.choices[i];
+        }
+        questionNum++;
+    }
+}
+
+function checkAnswer(answer) {
+    console.log(answer);
 }
 
 document.querySelector("#quizStart").onclick = startQuiz;
-document.querySelector(".btnOption").onclick = changeQuestion;
+document.addEventListener("click", changeQuestion);
+
