b1b9acd74e8a269614bbc73df7d93dcf4e49fd7f
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-08 01:05:25 -0400

    Added more questions to the array

--- questions.js
+++ questions.js
@@ -1,13 +1,33 @@
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
     },
     ///etc.
-  ];
+    {
+        title: "Data is stored in localStorage as _____.",
+        choices: ["strings", "objects", "arrays", "all of the above"],
+        answer: "strings"
+    },
+    {
+        title: "Which of the following is NOT a method to call an element with class 'example'?",
+        choices: ["document.getElementsByClassName('example');", "document.body.example;", "document.querySelector('.example')", "document.querySelectorAll('.example')[0]"],
+        answer: "document.body.example;"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ______.",
+        choices: ["strings", "numbers", "objects", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {
+        title: "Undefined does NOT mean that _______.",
+        choices: ["the variable in the code doesn't exist", "the variable is not assigned to a value", "the variable is assigned no value", "the property doesn't exist"],
+        answer: "the variable is assigned no value"
+    }
+];
